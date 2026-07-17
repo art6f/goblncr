b5731d191df9ed448d9556129ac5c7ad2a7d40fc
Artem O. <azure.email@gmail.com>
2026-07-17 19:49:57 +0200

    name/address

--- app/internal/balancer/pods-map.go
+++ app/internal/balancer/pods-map.go
@@ -5,26 +5,26 @@ import "net/netip"
 type PodsMap map[string]netip.Addr
 
 // Add an IP address for the Pod by name
-func (podsMap *PodsMap) AddAddress(name string, ip string) {
-
+func (podsMap *PodsMap) AddAddress(name string, ip string) error {
+	return nil
 }
 
 // Update a Pod name, but keep the associated IP address
-func (podsMap *PodsMap) UpdateName(oldName string, newName string) {
-
+func (podsMap *PodsMap) UpdateName(oldName string, newName string) error {
+	return nil
 }
 
 // Update an IP address associated with a Pod
-func (podsMap *PodsMap) UpdateAddress(name string, ip string) {
-
+func (podsMap *PodsMap) UpdateAddress(name string, ip string) error {
+	return nil
 }
 
 // Delete a Pod by name
-func (podsMap *PodsMap) DropByName(name string) {
-
+func (podsMap *PodsMap) DropName(name string) error {
+	return nil
 }
 
 // Delete a Pod by an associated IP address
-func (podsMap *PodsMap) DropByAddress(ip string) {
-
+func (podsMap *PodsMap) DropAddress(ip string) error {
+	return nil
 }
