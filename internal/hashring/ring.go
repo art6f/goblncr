2067ec34fbbbea33802c019dded99611d11d72ca
Artem O. <azure.email@gmail.com>
2026-09-11 10:56:06 +0200

    added a ring with add/delete

--- internal/hashring/ring.go
+++ internal/hashring/ring.go
@@ -2,13 +2,14 @@ package hashring
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/cespare/xxhash"
 )
 
 type Ring struct {
 	nodesMap map[uint64]string
-	ring     []string
+	ring     []uint64
 	vnodes   uint64
 }
 
@@ -16,7 +17,7 @@ func NewEmptyHashRing(vnodes uint64) *Ring {
 	return &Ring{
 		vnodes:   vnodes,
 		nodesMap: map[uint64]string{},
-		ring:     []string{},
+		ring:     []uint64{},
 	}
 }
 
@@ -30,17 +31,34 @@ func NewHashRing(servers []string, vnodes uint64) *Ring {
 	return ring
 }
 
-func (ring *Ring) AddNode(node string) {
+func (ring *Ring) AddNode(newNode string) {
 	for vnodeId := range ring.vnodes {
-		vnodeKey := fmt.Sprintf("node-%s-%d", node, vnodeId)
-		vnodeHash := ring.hashKey(vnodeKey)
+		nodeKey := fmt.Sprintf("node-%s-%d", newNode, vnodeId)
+		nodeHash := ring.hashKey(nodeKey)
 
-		ring.nodesMap[vnodeHash] = node
+		ring.nodesMap[nodeHash] = newNode
+		ring.ring = append(ring.ring, nodeHash)
 	}
+
+	sort.Slice(ring.ring, func(i, j int) bool {
+		return ring.ring[i] < ring.ring[j]
+	})
 }
 
-func (ring *Ring) RemoveNode(node string) {
+func (ring *Ring) RemoveNode(targetNode string) {
+	newMap := make(map[uint64]string, 0)
+	newRing := make([]uint64, 0)
 
+	for _, nodeHash := range ring.ring {
+		currNode := ring.nodesMap[nodeHash]
+
+		if currNode != targetNode {
+			newRing = append(newRing, nodeHash)
+			newMap[nodeHash] = currNode
+		}
+	}
+
+	ring.nodesMap, ring.ring = newMap, newRing
 }
 
 func (ring *Ring) GetNode(key string) string {
--- internal/hashring/ring_test.go
+++ internal/hashring/ring_test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestDistribution(t *testing.T) {
-	newVar := []string{"node1", "node2", "node3"}
-	ring := NewHashRing(newVar, 10)
-
-	assert.Equal(t, 30, ring.ring)
+	testNodes := []string{"node1", "node2", "node3"}
+	ring := NewHashRing(testNodes, 10)
+	assert.Len(t, ring.ring, 30)
+	
+	ring.RemoveNode("node1")
+	assert.Len(t, ring.ring, 20)
 }
