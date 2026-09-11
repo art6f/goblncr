b9e9c81bdb5d8b836c0c3482e25f0fd3431d9410
Artem O. <azure.email@gmail.com>
2026-09-11 10:56:36 +0200

    formatting

--- internal/hashring/ring_test.go
+++ internal/hashring/ring_test.go
@@ -9,7 +9,7 @@ func TestDistribution(t *testing.T) {
 	testNodes := []string{"node1", "node2", "node3"}
 	ring := NewHashRing(testNodes, 10)
 	assert.Len(t, ring.ring, 30)
-	
+
 	ring.RemoveNode("node1")
 	assert.Len(t, ring.ring, 20)
 }
